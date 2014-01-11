ef198786ccf3d601164a125df4a07b3f4309bbfe
CvK <valantis@cvk.gr>
2014-01-11 14:57:12 +0200

    remove turbolinks and nprogress

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,6 +1,3 @@
 //= require jquery
-//= require turbolinks
-//= require nprogress
-//= require nprogress-turbolinks
 //= require bootstrap
 //= require main
--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -6,28 +6,28 @@ var Application = {};
 // Initialize
 Application.initialize = function ()
 {
-  console.log('[Application::initialize] Intializing Application');
+  //console.log('[Application::initialize] Intializing Application');
 
   $(document).prepareQrcode();
 }
 // Fetch Page
-Application.fetchPage = function ()
-{
-  console.log('[Turbolinks] Fetching page...');
-  NProgress.start();
-}
+//Application.fetchPage = function ()
+//{
+//  console.log('[Turbolinks] Fetching page...');
+//  NProgress.start();
+//}
 // Change Page
-Application.changePage = function ()
-{
-  console.log('[Turbolinks] Page changed!');
-  NProgress.done();
-}
+//Application.changePage = function ()
+//{
+//  console.log('[Turbolinks] Page changed!');
+//  NProgress.done();
+//}
 // Restore Page
-Application.restorePage = function ()
-{
-  console.log('[Turbolinks] Page restored!');
-  NProgress.remove();
-}
+//Application.restorePage = function ()
+//{
+//  console.log('[Turbolinks] Page restored!');
+//  NProgress.remove();
+//}
 
 
 $.fn.prepareQrcode = function ()
@@ -70,17 +70,17 @@ $(document).ready(
 /*
     Turbolinks
 */
-$(document)
-  .on('page:load', Application.initialize)
-  .on('page:fetch', Application.fetchPage)
-  .on('page:change', Application.changePage)
-  .on('page:restore', Application.restorePage);
+//$(document)
+//  .on('page:load', Application.initialize)
+//  .on('page:fetch', Application.fetchPage)
+//  .on('page:change', Application.changePage)
+//  .on('page:restore', Application.restorePage);
 
 /*
     NProgress
 */
-NProgress.configure(
-  {
-    showSpinner: false
-  }
-);
+//NProgress.configure(
+//  {
+//    showSpinner: false
+//  }
+//);
