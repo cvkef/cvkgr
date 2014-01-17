3073276c921a90cd3685cb476e990e7d4f12c68f
CvK <valantis@cvk.gr>
2014-01-17 20:09:45 +0200

    add email config example

---
+++ source/config-example.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Email Configuration
+    $mail_host = 'ssl://smtp.example.com';
+    $mail_port = 465;
+    $mail_user = 'john_doe@example.com';
+    $mail_pass = 'password';
+    $mail_name = 'John Doe';
+    $mail_from = 'john_doe@example.com';
